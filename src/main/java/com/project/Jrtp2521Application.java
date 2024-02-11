94631667305b08b356a9df7cd7ad21a5f699c896
aniket <aniketkandrikar29@gmail.com>
2024-02-11 09:31:32 +0530

    uploadStruture

---
+++ src/main/java/com/project/Jrtp2521Application.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jrtp2521Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jrtp2521Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/Jrtp2521ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jrtp2521ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
